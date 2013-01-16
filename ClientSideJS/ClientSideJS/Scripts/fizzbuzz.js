97950bc043dc8870d2ff07b03f7bd1f2560c4f97
Alex Brown <alex@alexjamesbrown.com>
2013-01-16 23:33:00 +0000

    first test passing.

---
+++ ClientSideJS/ClientSideJS/Scripts/fizzbuzz.js
@@ -0,0 +1,10 @@
+﻿window.FizzBuzz = FizzBuzz = {
+    get: function(start, end, cb) {
+        var result = [];
+        for (var i = start; i < end; i++) {
+            result.push(i);
+        }
+
+        cb(result);
+    }
+}
--- ClientSideJS/ClientSideJS/Test/test.fizzbuzz.js
+++ ClientSideJS/ClientSideJS/Test/test.fizzbuzz.js
@@ -1,1 +1,10 @@
-﻿
+﻿describe("FizzBuzz", function () {
+
+    describe("when asking for numbers 1-10", function () {
+        it("returns 10 items", function () {
+            FizzBuzz.get(1, 10, function (result) {
+                assert(result.length = 10);
+            });
+        });
+    });
+});
