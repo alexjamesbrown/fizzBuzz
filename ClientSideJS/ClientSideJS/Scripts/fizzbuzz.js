64ddebd35e66553952337844096152dce591696e
Alex Brown <alex@alexjamesbrown.com>
2013-01-17 00:01:42 +0000

    refactored to allow fizz buzz numbers to be variable

--- ClientSideJS/ClientSideJS/Scripts/fizzbuzz.js
+++ ClientSideJS/ClientSideJS/Scripts/fizzbuzz.js
@@ -1,13 +1,15 @@
 ﻿window.FizzBuzz = FizzBuzz = {
     get: function(start, end, cb) {
         var result = [];
-        
+        var fizzNum = 3;
+        var buzzNum = 5;
+
         for (var i = start; i <= end; i++) {
-            if (i % 15 === 0)
+            if (i % (fizzNum*buzzNum) === 0)
                 result.push("fizzBuzz");
-            else if (i % 3===0)
+            else if (i % fizzNum===0)
                 result.push("fizz");
-            else if(i % 5===0)
+            else if(i % buzzNum===0)
                 result.push("buzz");
             else
                 result.push(i);
