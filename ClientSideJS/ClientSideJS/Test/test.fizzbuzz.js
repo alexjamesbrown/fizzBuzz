4469226e1b2b14ea6969fedd43a1c3b8901d0d20
Alex Brown <alex@alexjamesbrown.com>
2013-01-17 00:15:05 +0000

    refactored test

--- ClientSideJS/ClientSideJS/Test/test.fizzbuzz.js
+++ ClientSideJS/ClientSideJS/Test/test.fizzbuzz.js
@@ -7,19 +7,35 @@
             });
         });
 
-        it("third item should be fizz", function() {
-            FizzBuzz.get(1, 10, function(result) {
+        it("item in result with index of specified fizzNum should be fizz", function () {
+            FizzBuzz.get(1, 10, function (result) {
+                assert(result[FizzBuzz.fizzNum - 1] === "fizz");
+            });
+        });
+
+        it("item in result with index of specified buzzNum should be buzz", function () {
+            FizzBuzz.get(1, 10, function (result) {
+                assert(result[FizzBuzz.buzzNum - 1] === "buzz");
+            });
+        });
+    });
+
+    describe("when asking for numbers 1-10, specifying fizzNum as 3, and buzzNum as 5", function () {
+        it("item 3 should be fizz", function () {
+            FizzBuzz.get(1, 10, function (result) {
                 assert(result[2] === "fizz");
             });
         });
-        
-        it("fifth item should be buzz", function () {
+
+        it("item 5 should be buzz", function () {
             FizzBuzz.get(1, 10, function (result) {
                 assert(result[4] === "buzz");
             });
         });
 
-        it("has correct sequence", function() {
+        it("has correct sequence", function () {
+            FizzBuzz.fizzNum = 3;
+            FizzBuzz.fizzNum = 5;
             FizzBuzz.get(1, 10, function (result) {
                 result[0] == "1";
                 result[1] == "2";
@@ -34,15 +50,15 @@
             });
         });
     });
-    
+
     describe("when asking for numbers 1-15", function () {
-        it("item 15 is fizzBuzz", function() {
-            FizzBuzz.get(1, 15, function(result) {
+        it("item 15 is fizzBuzz", function () {
+            FizzBuzz.get(1, 15, function (result) {
                 assert(result[14] === "fizzBuzz");
             });
         });
     });
-    
+
     describe("when asking for numbers 1-150", function () {
         it("item 150 is fizzBuzz", function () {
             FizzBuzz.get(1, 150, function (result) {
